b868bf917d4a4175601b53724b38e64bddce3b2a
pramodniroshan <pramodnakandara@gmail.com>
2023-06-18 22:33:09 +0530

    codify route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const exampleRoutes = require('../src/routes/exampleRoutes');
+
 const app = express();
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World new!');
 });
+
+app.use('/example', exampleRoutes);
 
 const port = 8080; // Choose a port number where your server will listen
 
