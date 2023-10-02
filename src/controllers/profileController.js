6741b5dcc9704af26bc0d73382b0349942c4bb27
pramodniroshan <pramodnakandara@gmail.com>
2023-10-02 22:19:01 +0530

    new changes

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -44,15 +44,42 @@ export const createProfile = async (req, res) => {
 
 export const getProfile = async (req, res) => {
     const { userId } = req.params; // Extract userId from req.params
-    console.log(userId);
+    
     try {
         const profile = await Profile.findOne({ userId }); // Fetch the profile using the userId
-
+        console.log(profile,'uuuuuuuuuuuuuu');
         if (!profile) {
             return res.status(404).json({ success: false, error: 'Profile not found' });
         }
 
         res.status(200).json({ success: true, profile });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Something went wrong' });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    const { userId } = req.params; 
+    
+    try {
+        const existingProfile = await Profile.findOne({ userId });
+
+        if (!existingProfile) {
+            return res.status(404).json({ success: false, error: 'Profile not found' });
+        }
+
+        existingProfile.username = req.body.username;
+        existingProfile.city = req.body.city;
+        existingProfile.district = req.body.district;
+        existingProfile.race = req.body.race;
+        existingProfile.gender = req.body.gender;
+        existingProfile.religion = req.body.religion;
+
+      
+        await existingProfile.save();
+
+        res.status(200).json({ success: true, message: 'Profile updated successfully', profile: existingProfile });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: 'Something went wrong' });
--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import {createProfile,getProfile} from '../controllers/profileController.js';
+import {createProfile,getProfile,updateProfile} from '../controllers/profileController.js';
 
 const router = express.Router();
 
 
 router.post('/createProfile', createProfile);
 router.get('/getProfile/:userId', getProfile);
+router.post('/updateProfile/:userId', updateProfile);
 
 
 export default router;
